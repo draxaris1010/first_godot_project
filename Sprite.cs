c8140c12c278f7c78c0f349e5042cb9e1636cbc4
Axel <axel.dezittere@gmail.com>
2020-08-11 15:48:23 +0200

    last update

--- Sprite.cs
+++ Sprite.cs
@@ -10,12 +10,24 @@ public class Sprite : Godot.Sprite
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		float amount = 5;
+		if (Input.IsKeyPressed((int)KeyList.Z)){
+			this.Position += new Vector2(0, -amount)
+		}
+		if (Input.IsKeyPressed((int)KeyList.S)){
+			this.Position += new Vector2(0, amount)
+		}
+		if (Input.IsKeyPressed((int)KeyList.Q)){
+			this.Position += new Vector2(-amount, 0)
+		}
+		if (Input.IsKeyPressed((int)KeyList.D)){
+			this.Position += new Vector2(amount, 0)
+		}
+	}
 }
